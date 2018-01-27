1d1141534b6d3b270bee5d3b6b14f8c38edee209
Rafael Ramos <rrafaelramos2@gmail.com>
2018-01-27 01:39:57 -0200

    Diagrama de Sequencia

---
+++ Codigo Fonte/SIGPA_aplicacao/src/br/edu/ifnmg/tads/psc/sigpa/aplicacao/CaixaRepositorio.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.psc.sigpa.aplicacao;
+
+/**
+ *
+ * @author petronio
+ */
+public interface CaixaRepositorio extends Repositorio<Caixa> {
+    
+}
